de6fe5c26d0d5a1e48a5366268d76264be54be67
luiscarp <luisc.rodriguezpacheco@gmail,com>
2023-12-09 12:45:06 -0600

    landing page 1

---
+++ components/Cinta.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Cinta() {
+  return (
+    <div className=' w-full flex flex-row items-start'>
+        <div className=' flex flex-row items-center justify-center bg-sky-950 w-full'>
+            <h1 className=' text-white font-bold text-6xl py-8'>
+                Califica a tu asesor de tesis
+            </h1>
+
+        </div>
+      
+    </div>
+  )
+}
+
+export default Cinta
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,20 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import BtnLogin from '@/components/BtnLogin'
+import Cinta from '@/components/Cinta'
 
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <main>
-      <BtnLogin></BtnLogin>
-
+    <main >
+      <div className=" h-screen bg-[url('/landing.png')]  bg-cover bg-center bg-no-repeat">
+        <Cinta></Cinta>
+      <BtnLogin ></BtnLogin>
+      </div>
+    
+      
 
     </main>
   )
