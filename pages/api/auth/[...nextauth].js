841134f736946d053b72332a62645aef95fc4442
luiscarp <luisc.rodriguezpacheco@gmail,com>
2023-12-08 22:22:36 -0600

    autenticacion google-github

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth"
 import GitHubProvider from "next-auth/providers/github"
+import GoogleProvider from "next-auth/providers/google";
 
 export default NextAuth({
   providers: [
@@ -7,5 +8,11 @@ export default NextAuth({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
     }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET
+    })
+    
+    
   ],
 })
--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,14 @@ import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import BtnLogin from '@/components/BtnLogin'
 
+
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <main>
       <BtnLogin></BtnLogin>
+
 
     </main>
   )
