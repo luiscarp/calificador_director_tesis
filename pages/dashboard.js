949d09e428240ff979fa2c23e3c5392462548a31
luiscarp <luisc.rodriguezpacheco@gmail,com>
2023-12-09 23:29:22 -0600

    dashboard bug m

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,23 +1,9 @@
 import React from 'react'
-import { useSession, signIn, signOut } from "next-auth/react";
+
 import NavBar from '@/components/NavBar';
 
 function dashboard() {
-    const { data: session, status } = useSession()
-    if (status === "loading") {
-        return <span>Loading ... </span>
-      }
-    
-      if (session) {
-        return (
 
-          <div className=" h-screen bg-[url('/fondodash.png')]  bg-cover bg-center ">
- <NavBar></NavBar>
-          </div>
-         
-
-       
-        )}
   return (
     <div className=" h-screen bg-[url('/fondodash.png')]  bg-cover bg-center ">
        <NavBar></NavBar>
