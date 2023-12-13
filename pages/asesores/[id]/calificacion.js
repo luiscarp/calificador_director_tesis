5d1dcd2cb346c86d201e40a1785de151085e8fbe
luiscarp <luisc.rodriguezpacheco@gmail,com>
2023-12-13 01:32:02 -0600

    errfix2

--- pages/asesores/[id]/calificacion.js
+++ pages/asesores/[id]/calificacion.js
@@ -1,12 +1,17 @@
+import { useRouter } from "next/router";
+
+function Calificacion() {
+    const router = useRouter();
+    // Obtener el ID de la universidad de la URL
 
 
-function calificacion() {
-
+    const { id } = router.query;
+    
   return (
     <div>
-      <h1>Aqui iran las calificaciones de cada asesor seleccionado </h1>
+      <h1>Aqui iran las calificaciones de cada asesor seleccionado {id}</h1>
     </div>
   )
 }
 
-export default calificacion
+export default Calificacion
