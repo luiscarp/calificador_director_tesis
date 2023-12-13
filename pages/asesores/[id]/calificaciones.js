c2d69c3cb4fb359833ad27421084a0fffd2ed912
luiscarp <luisc.rodriguezpacheco@gmail,com>
2023-12-13 01:39:48 -0600

    id calis

--- pages/asesores/[id]/calificaciones.js
+++ pages/asesores/[id]/calificaciones.js
@@ -1,9 +1,16 @@
 import React from 'react'
 
+import { useRouter } from 'next/router'
+
 function Calificaciones() {
+    const router = useRouter();
+    // Obtener el ID de la universidad de la URL
+
+
+    const { id } = router.query;
   return (
     <div>
-      <h1>Aqui van las calis</h1>
+      <h1>Aqui van las calis {id}</h1>
     </div>
   )
 }
