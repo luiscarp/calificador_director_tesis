1a0990e49653629a3f0173fafbf9521b901bbb2d
luiscarp <luisc.rodriguezpacheco@gmail,com>
2023-12-15 12:00:39 -0600

    Eliminar cinta inferior login screen

--- components/LoginScreen.js
+++ components/LoginScreen.js
@@ -24,12 +24,12 @@ function LoginScreen() {
           <Link className=" mt-3 bg-blue-200 text-sky-950 p-2 font-bold text-lg shadow shadow-black rounded-md md:text-2xl" href="/dashboard"> Ir al dashboard </Link>
 
         </div>
-        <div className=" w-full flex flex-col  bg-sky-950 items-center mt-4 py-5 text-white mx-auto text-lg font-bold justify-center md:w-full md:text-2xl">
-          Bienvenido, {session.user.name}
+        <div className=" w-full flex flex-col text-sky-950 items-center mt-4 py-5 mx-auto text-md font-bold justify-end  md:text-2xl">
+          
 
 
 
-          <button className=" mt-3 bg-blue-200 text-sky-950 p-2 rounded-md shadow-black shadow" onClick={() => signOut()}>Cerrar sesión</button>
+          <button className=" mt-3 mx-5 bg-blue-200 text-sky-950 p-2 rounded-md shadow-black shadow" onClick={() => signOut()}>Bienvenido, {session.user.name} <br></br> Cerrar sesión</button>
 
         </div>
 
